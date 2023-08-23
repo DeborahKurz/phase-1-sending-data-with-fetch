6025067dd10bb7a421b14f5dc8eec2cc24930719
Deborah Kurz <DeborahKurz6@gmail.com>
2023-08-23 12:58:31 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
-// Add your code here
+// function submitData(personsName, personsEmail){
+//     return fetch("http://localhost:3000/users", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             name: `${personsName}`,
+//             email: `${personsEmail}`,
+//         })
+//     });
+// };
+
+// function submitData(){
+//     return fetch("http://localhost:3000/users",{
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             name: "Steve",
+//             email: "steve@steve.com",
+//         })
+//     })
+    // .then(function (response){
+    //     return response.json()
+    // })
+    // .then(function (submitData){
+    //     const paragraph = document.body.createElement("p");
+    //     paragraph.textContent = submitData.id
+    // })
+    // .catch(function (error) {
+    //     alert("Unauthorized Access");
+    //     console.log(error.message);
+    // });
+// };
+
+// fetch("http://localhost:3000/users", submitData)
+//     .then(function (response){
+//         return response.json()
+//     })
+//     .then(function (submitData){
+//         // console.log(submitData)
+//         // document.body.append(submitData[4].id)
+//         const paragraph = document.body.createElement("p");
+//         paragraph.textContent = submitData.id
+//     })
+//     .catch(function (error) {
+//         alert("A problem occured");
+//         console.log(error.message);
+//     });
+
+
+function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`,
+        })
+    })
+    .then(res => res.json())
+    .then(data =>{
+        const body = document.querySelector("body")
+        body.innerHTML = data.id
+    })
+    .catch(function(error){
+        const message = document.querySelector("body")
+        message.innerHTML = error.message;
+    })
+}
